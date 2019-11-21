d13492433dbfe4ece4d2daea52517d330342e5d9
Yaobo Xu <yx2@sanger.ac.uk>
2019-11-21 13:05:39 +0000

    remove "print" lines added earlier for debugging

--- parabam/core.pyx
+++ parabam/core.pyx
@@ -89,7 +89,6 @@ cdef class Handler:
         self._report = report
         self._stats = {}
         self._constants = constants
-        print "parabam - _constants.total_procs", self._constants.total_procs
         self._verbose = constants.verbose
         self._output_paths = output_paths
 
