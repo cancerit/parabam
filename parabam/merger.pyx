0588117cc946dbc87c69a336a5ff3f8934732964
Yaobo Xu <yx2@sanger.ac.uk>
2019-12-03 01:21:00 +0000

    removed some prints

--- parabam/merger.pyx
+++ parabam/merger.pyx
@@ -282,7 +282,6 @@ class Handler(parabam.core.Handler):
             return ready_to_merge
 
     def __dump_to_BAM_file__(self,merge_path,subset):
-        print "\nin __dump_to_BAM_file__"
         with open(merge_path,"rb") as merge_file:
             if self._subset_has_header[subset]:
                 merge_file.seek(self.__get_header_location__(merge_path))
@@ -294,22 +293,17 @@ class Handler(parabam.core.Handler):
                 self._out_file_objects[subset].write(binary_data)
                 self._out_file_objects[subset].flush()
         os.remove(merge_path)
-        print "\nout __dump_to_BAM_file__"
 
     def __get_binary_from_file__(self,open_file):
         prev_data = ""
         i = 0
 
-        print "\nin __get_binary_from_file__"
-        sys.stdout.flush()
         while True:
             binary_data = open_file.read(2**19)
             if not i == 0:
                 combined = prev_data+binary_data
                 if combined[len(combined)-28:] == self._EOF_SIGNATURE:
                     yield combined[:len(combined)-28]
-                    print "\nout __get_binary_from_file__"
-                    sys.stdout.flush()
                     return
                 else:
                     yield prev_data
