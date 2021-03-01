24c983bdb8fbb904e2fcf3d99d2d15691969bebc
Keiran Raine <kr2@sanger.ac.uk>
2021-03-01 10:14:59 +0000

    more setup cleanup

--- setup.py
+++ setup.py
@@ -3,39 +3,32 @@ import sys
 from setuptools import Extension, setup
 from setuptools.command.sdist import sdist as _sdist
 
-cmdclass = {}
-ext_modules = [
-  Extension("parabam.core", [ "parabam/core.c" ]),
-  Extension("parabam.chaser", ["parabam/chaser.c"]),
-  Extension("parabam.merger", ["parabam/merger.c"]),
-  Extension("parabam.command.subset", ["parabam/command/subset.c"]),
-  Extension("parabam.command.stat", ["parabam/command/stat.c"]),
-  Extension("parabam.command.core", ["parabam/command/core.c"])]
-
 class sdist(_sdist):
   def run(self):
     # Make sure the compiled Cython files in the distribution are up-to-date
     from Cython.Build import cythonize
-    cythonize(['parabam/chaser.pyx','parabam/core.pyx','parabam/merger.pyx',
-      'parabam/command/core.pyx','parabam/command/stat.pyx',
-      'parabam/command/subset.pyx'],
+    cythonize(['parabam/chaser.pyx','parabam/core.pyx','parabam/merger.pyx','parabam/command/core.pyx',
+      'parabam/command/stat.pyx','parabam/command/subset.pyx'],
       compiler_directives={'language_level' : '3'})
     _sdist.run(self)
 
-cmdclass['sdist'] = sdist
-
-require_modules = ['numpy','argparse','pysam >= 0.10.0']
-
 setup(name='parabam',
-  description='Parallel BAM File Analysis',
   version='2.3.2',
+  description='Parallel BAM File Analysis',
   author="JHR Farmery",
   license='GPL',
   author_email = 'cgphelp@sanger.ac.uk',
   packages = ['parabam','parabam.command'],
   package_dir = {'parabam':'parabam','parabam.command':'parabam/command'},
-  install_requires = require_modules,
+  install_requires = ['numpy','argparse','pysam >= 0.10.0'],
   scripts = ['parabam/bin/parabam'],
-  cmdclass = cmdclass,
-  ext_modules=ext_modules
+  cmdclass = {'sdist': sdist},
+  ext_modules=[
+    Extension("parabam.core", [ "parabam/core.c" ]),
+    Extension("parabam.chaser", ["parabam/chaser.c"]),
+    Extension("parabam.merger", ["parabam/merger.c"]),
+    Extension("parabam.command.subset", ["parabam/command/subset.c"]),
+    Extension("parabam.command.stat", ["parabam/command/stat.c"]),
+    Extension("parabam.command.core", ["parabam/command/core.c"])
+  ]
 )
